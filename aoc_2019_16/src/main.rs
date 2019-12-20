47c1b1f41e1fb8c1f9a728d9a4302c62177e52b9
Zichun Koh <zichun@gmail.com>
2019-12-19 23:12:03 -0800

    day 16 part 2

--- aoc_2019_16/src/main.rs
+++ aoc_2019_16/src/main.rs
@@ -10,6 +10,7 @@ fn main() -> Result<()> {
     io::stdin().read_line(&mut input)?;
 
     println!("part1: {}", part1(&input, 100)?);
+    println!("part2: {}", part2(&input, 100)?);
     Ok(())
 }
 
@@ -73,7 +74,7 @@ fn part1(input: &str, phases: usize) -> Result<String> {
 
 fn parse_input_part2(input: &str) -> Vec<u8> {
     let base_input: Vec<u32> = input.chars().filter_map(|x| x.to_digit(10)).collect();
-    let tr: Vec<u32> = Vec::new();
+    let mut tr: Vec<u32> = Vec::new();
     for i in 0..10000 {
         tr.extend(base_input.iter());
     }
@@ -81,11 +82,45 @@ fn parse_input_part2(input: &str) -> Vec<u8> {
 }
 
 fn part2(input: &str, phases: usize) -> Result<String> {
-    let new_input = parse_input_part2(input);
+    let mut new_input = parse_input_part2(input);
+    let skip_string: String = new_input.as_slice()[0..7].iter().map(|x| std::char::from_digit(*x as u32, 10).unwrap() ).collect();
+    let skip = skip_string.parse::<usize>()?;
 
     for i in 1..=phases {
-        
+        let mut next_input = Vec::new();
+        let mut prefix_sum: Vec<i64> = Vec::new();
+        prefix_sum.push(new_input[0] as i64);
+        for j in 1..new_input.len() {
+            prefix_sum.push( prefix_sum[j - 1] + new_input[j] as i64 );
+        }
+        for j in 1..=new_input.len() {
+            let mut start = j - 1;
+            let mut sum: i64 = 0;
+            let mut add = true;
+
+            while start < new_input.len() {
+                let segment = if start == 0 {
+                    prefix_sum[ usize::min(prefix_sum.len() - 1, start + j - 1) ]
+                } else {
+                    prefix_sum[ usize::min(prefix_sum.len() - 1, start + j - 1) ] - prefix_sum[start - 1]
+                };
+
+                if add {
+                    sum = sum + segment;
+                } else {
+                    sum = sum - segment;
+                }
+
+                start = start + j + j;
+                add = !add;
+            }
+            next_input.push( (i64::abs(sum) % 10) as u8 );
+        }
+        new_input = next_input;
     }
+
+    let output_string: String = new_input.as_slice()[skip..skip+8].into_iter().take(8).map(|x| std::char::from_digit(*x as u32, 10).unwrap() ).collect();
+    Ok(output_string)
 }
 
 #[cfg(test)]
@@ -98,5 +133,12 @@ mod tests {
         assert_eq!(part1("80871224585914546619083218645595", 100).unwrap(), "24176176");
         assert_eq!(part1("19617804207202209144916044189917", 100).unwrap(), "73745418");
         assert_eq!(part1("69317163492948606335995924319873", 100).unwrap(), "52432133");
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part2("03036732577212944063491565474664", 100).unwrap(), "84462026");
+        assert_eq!(part2("02935109699940807407585447034323", 100).unwrap(), "78725270");
+        assert_eq!(part2("03081770884921959731165446850517", 100).unwrap(), "53553731");
     }
 }
